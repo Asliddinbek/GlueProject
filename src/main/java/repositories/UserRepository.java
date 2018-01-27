d3c149d867f2ea3e777d1916e894e57c39b8be71
Glue <tiuasliddin@umail.com>
2018-01-28 00:37:36 +0500

    Commitcha

---
+++ src/main/java/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.Glue.GlueProject.model.User;
+ 
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+	 User findByEmail(String email);
+}
